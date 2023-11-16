ebbc913bd40b8df0e816cf65153a3658f32d13a1
Svitly4nyi-Vla2yslav <126347711+Svitly4nyi-Vla2yslav@users.noreply.github.com>
2023-11-16 22:16:04 +0100

    httpError

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -2,21 +2,32 @@ const express = require('express')
 
 const contacts = require("../../models/contacts")
 
+const { HttpError } = require("../../helpers")
+
 const router = express.Router()
 
-router.get('/', async (req, res) => {
+router.get('/', async (req, res, next) => {
   try {
     const result = await contacts.listContacts();
     res.json(result)
   } catch (error) {
-    res.status(500).json({ message: "Server error" })
+    next(error)
   }
 
 })
 
 router.get('/:contactId', async (req, res, next) => {
-  
-  res.json({ message: 'template message' })
+  try {
+    const { contactId } = req.params;
+    const result = await contacts.getContactById(contactId);
+    if (!result) {
+      throw HttpError(404, "Not found")
+    }
+    res.json(result);
+  } catch (error) {
+    next(error)
+  }
+
 })
 
 router.post('/', async (req, res, next) => {
