951367bfabbbca7e95b0aeac2a7d5e8dee9f9e75
Svitly4nyi-Vla2yslav <126347711+Svitly4nyi-Vla2yslav@users.noreply.github.com>
2023-11-20 23:20:38 +0100

    mongoose

--- server.js
+++ server.js
@@ -1,5 +1,19 @@
+const mongoose = require("mongoose")
+
+const DB_HOST = "mongodb+srv://vladyslav:12345678910@cluster0.be9jlmk.mongodb.net/contacts_reader?retryWrites=true&w=majority"
+
+mongoose.set('strictQuery', true)
+
+mongoose.connect(DB_HOST)
+.then(() => {
+  app.listen(3000)
+})
+.catch(error => {
+  console.log(error.message)
+  process.exit(1)
+})
+
+
 const app = require('./app')
 
-app.listen(3000, () => {
-  console.log("Server running. Use our API on port: 3000")
-})
+
