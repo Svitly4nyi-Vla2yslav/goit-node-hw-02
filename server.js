b963c1a5a1f4a0f443344b9bca445a96231ddf0e
Svitly4nyi-Vla2yslav <126347711+Svitly4nyi-Vla2yslav@users.noreply.github.com>
2023-11-22 21:09:16 +0100

    task-3

--- server.js
+++ server.js
@@ -1,12 +1,15 @@
 const mongoose = require("mongoose")
 const app = require('./app')
-const {DB_HOST} = process.env;
+const {DB_HOST, PORT = 3000} = process.env;
 
 mongoose.set('strictQuery', true)
-console.log(`Connecting to MongoDB at: ${DB_HOST}`);
+
+// console.log(`Connecting to MongoDB at: ${DB_HOST}`);
+
 mongoose.connect(DB_HOST)
+
 .then(() => {
-  app.listen(3000)
+  app.listen(PORT)
 })
 .catch(error => {
   console.log(error.message)
