713feea4fd8a07fa72e93c059cc6eb73550554b7
Svitly4nyi-Vla2yslav <126347711+Svitly4nyi-Vla2yslav@users.noreply.github.com>
2023-12-04 20:47:22 +0100

    fix routes

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -6,14 +6,14 @@ const ctrl = require("../../controllers/contacts")
 
 const router = express.Router();
 
-router.post("/users/register", validateBody(schemas.registerSchema), ctrl.register);
+router.post("/register", validateBody(schemas.registerSchema), ctrl.register);
 
-router.post("/users/login", validateBody(schemas.loginSchema), ctrl.login);
+router.post("/login", validateBody(schemas.loginSchema), ctrl.login);
 
-router.get("/users/current", authenticate, ctrl.getCurrent);
+router.get("/current", authenticate, ctrl.getCurrent);
 
-router.post("/users/logout", authenticate, ctrl.logout);
+router.post("/logout", authenticate, ctrl.logout);
 
-router.patch("/users", authenticate, validateBody(schemas.updateSubscriptionSchema), ctrl.updateSubscription)
+router.patch("/current", authenticate, validateBody(schemas.updateSubscriptionSchema), ctrl.updateSubscription)
 
 module.exports = router;
