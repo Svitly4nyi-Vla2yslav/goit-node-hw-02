81b2514061c83567379343f4e0075931a1eb61e6
Svitly4nyi-Vla2yslav <126347711+Svitly4nyi-Vla2yslav@users.noreply.github.com>
2023-12-05 19:06:48 +0100

    fix hw-04

--- controllers/contacts/authRegister.js
+++ controllers/contacts/authRegister.js
@@ -13,8 +13,11 @@ const register = async (req, res) => {
     const newUser = await User.create({ ...req.body, password: hashPassword });
 
     res.status(201).json({
-        email: newUser.email,
-        subscription: newUser.subscription,
+        user: {
+            email: newUser.email,
+            subscription: newUser.subscription,
+        }
+
     })
 };
 
