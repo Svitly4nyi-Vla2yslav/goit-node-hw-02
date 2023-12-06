8f6f6be8c1e08b496919435d69540354095d8c6e
Svitly4nyi-Vla2yslav <126347711+Svitly4nyi-Vla2yslav@users.noreply.github.com>
2023-12-06 22:23:22 +0100

    Jimp resize

--- controllers/contacts/authUpdateAvatar.js
+++ controllers/contacts/authUpdateAvatar.js
@@ -1,17 +1,21 @@
 const path = require("path");
 const fs = require("fs/promises")
 const { User } = require("../../models/user");
+const Jimp = require("jimp");
 const avatarsDir = path.join(__dirname, "../../", "public", "avatars");
 
 
 const updateAvatar = async(req, res)=> {
     const {_id} = req.user;
     const {path: tempUpload, originalname} = req.file;
+    const avatarIMG = await Jimp.read(tempUpload);
+    avatarIMG.resize(250, 250);
     const filename = `${_id}_${originalname}`;
     const resultUpload = path.join(avatarsDir, filename);
     await fs.rename(tempUpload, resultUpload);
     const avatarURL = path.join("avatars", filename);
     await User.findByIdAndUpdate(_id, {avatarURL});
+    await avatarIMG.writeAsync(resultUpload);
 
     res.json({
         avatarURL,
